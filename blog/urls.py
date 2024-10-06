d122fd0eb516569614f25369f79d1837a5276a4f
prianthonhugo <prianthonsubardiodanhugo@gmail.com>
2024-10-06 17:40:55 +0700

    blog app

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from blog.views import homepage
+
+urlpatterns = [
+    path('', homepage)
+]
---
+++ config/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+# Database Config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Override Installed Apps
+INSTALLED_APPS += [
+    'blog.apps.BlogConfig'
+]

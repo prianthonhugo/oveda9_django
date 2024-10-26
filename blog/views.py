7e1cff3255c5d8f67fc5be4c214f15108389a498
prianthonhugo <prianthonsubardiodanhugo@gmail.com>
2024-10-26 12:16:52 +0700

    24.48

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/homepage.html')
